9d675653a7b6b57fedfb24cd7646177328a36d15
Haneum-Park <qkrgksdma17@gmail.com>
2023-05-12 15:09:47 +0900

    [ FEAT ] : 버튼 추가중

--- src/kanueditor.js
+++ src/kanueditor.js
@@ -11,53 +11,108 @@ function resizerRender() {
 function contentRender() {
 	// ? editor content wrapper
 	const content = document.createElement("div");
+	content.contentEditable = true;
 	content.classList.add("kanu-editor-content");
+	content.tabIndex = 0;
 
 	return content;
 }
 
 function toolbarRender() {
+	const toolbarIcons = {
+		fmt: {
+			bold: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M8.21 13c2.106 0 3.412-1.087 3.412-2.823 0-1.306-.984-2.283-2.324-2.386v-.055a2.176 2.176 0 0 0 1.852-2.14c0-1.51-1.162-2.46-3.014-2.46H3.843V13H8.21zM5.908 4.674h1.696c.963 0 1.517.451 1.517 1.244 0 .834-.629 1.32-1.73 1.32H5.908V4.673zm0 6.788V8.598h1.73c1.217 0 1.88.492 1.88 1.415 0 .943-.643 1.449-1.832 1.449H5.907z"></path></svg>',
+				type: "button",
+				id: "font-bold",
+				class: "toolbar-font-bold",
+			},
+			italic: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M7.991 11.674 9.53 4.455c.123-.595.246-.71 1.347-.807l.11-.52H7.211l-.11.52c1.06.096 1.128.212 1.005.807L6.57 11.674c-.123.595-.246.71-1.346.806l-.11.52h3.774l.11-.52c-1.06-.095-1.129-.211-1.006-.806z"></path></svg>',
+				type: "button",
+				id: "font-italic",
+				class: "toolbar-font-italic",
+			},
+			underline: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M5.313 3.136h-1.23V9.54c0 2.105 1.47 3.623 3.917 3.623s3.917-1.518 3.917-3.623V3.136h-1.23v6.323c0 1.49-.978 2.57-2.687 2.57-1.709 0-2.687-1.08-2.687-2.57V3.136zM12.5 15h-9v-1h9v1z"></path></svg>',
+				type: "button",
+				id: "font-underline",
+				class: "toolbar-font-underline",
+			},
+			strike: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M6.333 5.686c0 .31.083.581.27.814H5.166a2.776 2.776 0 0 1-.099-.76c0-1.627 1.436-2.768 3.48-2.768 1.969 0 3.39 1.175 3.445 2.85h-1.23c-.11-1.08-.964-1.743-2.25-1.743-1.23 0-2.18.602-2.18 1.607zm2.194 7.478c-2.153 0-3.589-1.107-3.705-2.81h1.23c.144 1.06 1.129 1.703 2.544 1.703 1.34 0 2.31-.705 2.31-1.675 0-.827-.547-1.374-1.914-1.675L8.046 8.5H1v-1h14v1h-3.504c.468.437.675.994.675 1.697 0 1.826-1.436 2.967-3.644 2.967z"></path></svg>',
+				type: "button",
+				id: "font-strike",
+				class: "toolbar-font-strike",
+			},
+		},
+		align: {
+			left: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path></svg>',
+				type: "button",
+				id: "align-left",
+				class: "toolbar-align-left",
+			},
+			center: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path></svg>',
+				type: "button",
+				id: "align-center",
+				class: "toolbar-align-center",
+			},
+			right: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm4-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path></svg>',
+				type: "button",
+				id: "align-right",
+				class: "toolbar-align-right",
+			},
+		},
+		indent: {
+			increase: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm.646 2.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L4.293 8 2.646 6.354a.5.5 0 0 1 0-.708zM7 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm-5 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path></svg>',
+				type: "button",
+				id: "indent-increase",
+				class: "toolbar-indent-increase",
+			},
+			decrease: {
+				icon: '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M2 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm10.646 2.146a.5.5 0 0 1 .708.708L11.707 8l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zM2 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"></path></svg>',
+				type: "button",
+				id: "indent-decrease",
+				class: "toolbar-indent-decrease",
+			},
+		},
+		media: {
+			link: {},
+			image: {},
+			video: {},
+			audio: {},
+		},
+	};
+
 	// ? editor toolbar wrapper
 	const toolbar = document.createElement("div");
 	toolbar.classList.add("kanu-editor-toolbar");
 
 	// ? toolbar button wrapper
-	// TODO bold, italic, underline, strike
-	const btnGroup = document.createElement("div");
-	btnGroup.classList.add("kanu-editor-toolbar-btngroup");
-	const btnBold = document.createElement("button");
-	btnBold.type = "button";
-	btnBold.id = "font-bold";
-	btnBold.classList.add("toolbar-font-bold");
-	btnBold.textContent = "B";
-	const btnItalic = document.createElement("button");
-	btnItalic.type = "button";
-	btnItalic.id = "font-italic";
-	btnItalic.classList.add("toolbar-font-italic");
-	btnItalic.textContent = "I";
-	const btnUnderline = document.createElement("button");
-	btnUnderline.type = "button";
-	btnUnderline.id = "font-underline";
-	btnUnderline.classList.add("toolbar-font-underline");
-	btnUnderline.textContent = "U";
-	const btnStrike = document.createElement("button");
-	btnStrike.type = "button";
-	btnStrike.id = "font-strike";
-	btnStrike.classList.add("toolbar-font-strike");
-	btnStrike.textContent = "S";
+	Object.keys(toolbarIcons).forEach((key) => {
+		const btnGroup = document.createElement("div");
+		btnGroup.classList.add("kanu-editor-toolbar-btngroup");
+		btnGroup.id = `btn-${key}-group`;
 
-	// TODO align-left, align-center, align-right, align-justify
-	// TODO link, image, video
+		const btnObj = toolbarIcons[key];
+		Object.keys(btnObj).forEach((childKey) => {
+			const btn = document.createElement("button");
+			btn.type = btnObj[childKey].type;
+			btn.id = btnObj[childKey].id;
+			btn.classList.add(btnObj[childKey].class);
+			btn.innerHTML = btnObj[childKey].icon;
+			btnGroup.appendChild(btn);
+		});
+		toolbar.appendChild(btnGroup);
+	});
+
 	// TODO table, code, quote
 	// TODO ordered-list, unordered-list, horizontal-rule
 	// TODO undo, redo, clean
-
-	btnGroup.appendChild(btnBold);
-	btnGroup.appendChild(btnItalic);
-	btnGroup.appendChild(btnUnderline);
-	btnGroup.appendChild(btnStrike);
-
-	toolbar.appendChild(btnGroup);
 
 	return toolbar;
 }
